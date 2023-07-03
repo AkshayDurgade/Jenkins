b18e4f8a281a68289729f38aa4e944436d3d1035
AkshayDurgade <131943196+AkshayDurgade@users.noreply.github.com>
2023-07-03 17:53:11 +0530

    Update StarPattern.java

--- StarPattern.java
+++ StarPattern.java
@@ -1,6 +1,6 @@
 public class StarPattern {
     public static void main(String[] args) {
-        int rows = 11;
+        int rows = 5;
 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
